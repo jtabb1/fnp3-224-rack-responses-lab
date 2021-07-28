fa7538e01bc081561ea181fccb8e0748cdac6b13
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-28 12:48:27 -0400

    Include official solution as an alternate possible test passer.

--- app/application.rb
+++ app/application.rb
@@ -7,3 +7,20 @@ class Application
     Time.now.hour < 12 ?  ["Good Morning!"] : ["Good Afternoon!"]
   end
 end
+
+# The version given by the official solution may also work:
+
+# class Application
+
+#   def call(env)
+#     resp = Rack::Response.new
+#     now = Time.now
+#     if now.hour >=12
+#       resp.write "Good Afternoon"
+#     else
+#       resp.write "Good Morning"
+#     end
+#     resp.finish
+#   end
+
+# end
